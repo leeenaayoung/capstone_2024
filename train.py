f08a12e74790a19c2e008b06a284a2d60fa8167f
leeenaayoung <i01088847847@gmail.com>
2025-01-14 21:07:22 +0900

    분류 모델 가져와서 궤적 분류 후 생성하기(미완)

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-from models import *
 from dataloader import *
 import os
 import csv
@@ -10,12 +9,13 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, random_split
 # from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from functools import partial
-from models import TransformerModel
-from models import LSTMModel
-from utils import TrajectoryAnalyzer, TrajectoryGenerator
-from utils import *
+from model import TransformerModel, initialize_weights
+from sklearn.preprocessing import StandardScaler
+from tqdm import tqdm
+from torch import optim
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+scaler = StandardScaler()
 
 ##########################
 # 분류 모델 학습
@@ -62,93 +62,117 @@ def train_classification(model, train_loader, val_loader, criterion, optimizer, 
 
     print("Training complete.")
 
+
 ##########################
-# 생성 모델 학습
+# 테스트
 ##########################
-def train_generation(analyzer, movement_type, train_loader, val_loader, num_samples=10, epochs=100):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    golden_samples = []
-    for _ in range(num_samples):
-        try:
-            target_traj, _ = analyzer.get_random_trajectory_by_type(
-                analyzer.golden_dir, movement_type
-            )
-            golden_samples.append(target_traj)
-        except Exception as e:
-            print(f"Error collecting sample: {str(e)}")
-    
-    if not golden_samples:
-        raise ValueError("No golden samples could be collected")
-    
-    # 데이터셋 준비
-    combined_df = pd.concat(golden_samples, ignore_index=True)
-    dataset = GenerationDataset(combined_df)
-    
-    # 모델 초기화
-    input_size = len(dataset.features)
-    model = LSTMModel(input_size=input_size).to(device)
-    
-    # 손실 함수와 옵티마이저 정의
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
-    
-    # 학습 기록
-    history = {'train_loss': [], 'val_loss': []}
-    best_val_loss = float('inf')
-    
- 
-    for epoch in range(epochs):
-        # 훈련
-        model.train()
-        train_loss = 0
-        for batch_idx, (data, target) in enumerate(train_loader):
-            data, target = data.to(device), target.to(device)
-            
-            optimizer.zero_grad()
-            output = model(data)
-            loss = criterion(output, target)
-            
-            loss.backward()
-            optimizer.step()
-            
-            train_loss += loss.item()
-        
-        train_loss /= len(train_loader)        
-        
-        # 검증
-        model.eval()
-        val_loss = 0
-        with torch.no_grad():
-            for data, target in val_loader:
-                data, target = data.to(device), target.to(device)
-                output = model(data)
-                val_loss += criterion(output, target).item()
-        
-        val_loss /= len(val_loader)
-        
-        # 손실 기록
-        history['train_loss'].append(train_loss)
-        history['val_loss'].append(val_loss)
-        
-        print(f'Epoch {epoch+1}/{epochs}:')
-        print(f'Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}')
-        
-        # 베스트 모델 저장
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            # 모델 저장 전에 CPU로 이동
-            torch.save(model.cpu().state_dict(), 'best_generation_model.pth')
-            model.to(device)  # 다시 GPU로 이동
-    
-    # 베스트 모델 로드
-    state_dict = torch.load('best_generation_model.pth')
-    model.cpu()
-    model.load_state_dict(state_dict)
-    model.to(device)
-    return model, history, dataset.scaler
-    
+# def test_model(model, test_loader):
+#     model.eval()
+#     test_correct = 0
+#     total_test = 0
+#     all_labels_list = []
+#     all_preds_list = []
+
+#     with torch.no_grad():
+#         for data, labels in test_loader:
+#             data, labels = data.to(device), labels.to(device)
+#             outputs = model(data)
+#             _, predicted = torch.max(outputs, 1)
+
+#             test_correct += (predicted == labels).sum().item()
+#             total_test += labels.size(0)
+
+#             all_labels_list.extend(labels.cpu().numpy())
+#             all_preds_list.extend(predicted.cpu().numpy())
+
+#     test_acc = test_correct / total_test
+#     print(f"Test Accuracy: {test_acc:.4f}")
+
+#     # 혼동 행렬
+#     cm_normalized = confusion_matrix(all_labels_list, all_preds_list, normalize='true')
+#     disp = ConfusionMatrixDisplay(confusion_matrix=cm_normalized, display_labels=unique_labels)
+#     disp.plot(cmap=plt.cm.Blues)
+#     plt.title("Normalized Confusion Matrix")
+#     plt.show()
+
+######################
+# 단일 궤적 예측(분류 모델 테스트)
+######################
+def load_and_preprocess_trajectory(file_path, imported_scaler=None):
+    with open(file_path, 'r') as f:
+        reader = csv.reader(f)
+        data_list = list(reader)
+
+    df_t = pd.DataFrame(data_list, columns=[
+        'r', 'sequence', 'timestamp', 'deg', 'deg/sec', 'mA',
+        'endpoint', 'grip/rotation', 'torque', 'force', 'ori', '#'
+    ])
+
+    # 필터링
+    df_t = df_t[df_t['r'] != 's']
+    data_v = df_t.drop(['r', 'grip/rotation', '#'], axis=1)
+
+    # endpoint
+    v_split = data_v['endpoint'].astype(str).str.split('/')
+    data_v['x_end'] = v_split.str.get(0)
+    data_v['y_end'] = v_split.str.get(1)
+    data_v['z_end'] = v_split.str.get(2)
+
+    # deg
+    v_split = data_v['deg'].astype(str).str.split('/')
+    data_v['deg1'] = v_split.str.get(0)
+    data_v['deg2'] = v_split.str.get(1)
+    data_v['deg3'] = v_split.str.get(2)
+    data_v['deg4'] = v_split.str.get(3)
+
+    # deg/sec
+    v_split = data_v['deg/sec'].astype(str).str.split('/')
+    data_v['degsec1'] = v_split.str.get(0)
+    data_v['degsec2'] = v_split.str.get(1)
+    data_v['degsec3'] = v_split.str.get(2)
+    data_v['degsec4'] = v_split.str.get(3)
+
+    # torque
+    v_split = data_v['torque'].astype(str).str.split('/')
+    data_v['torque1'] = v_split.str.get(0)
+    data_v['torque2'] = v_split.str.get(1)
+    data_v['torque3'] = v_split.str.get(2)
+
+    # force
+    v_split = data_v['force'].astype(str).str.split('/')
+    data_v['force1'] = v_split.str.get(0)
+    data_v['force2'] = v_split.str.get(1)
+    data_v['force3'] = v_split.str.get(2)
+
+    # ori
+    v_split = data_v['ori'].astype(str).str.split('/')
+    data_v['yaw'] = v_split.str.get(0)
+    data_v['pitch'] = v_split.str.get(1)
+    data_v['roll'] = v_split.str.get(2)
+
+    # 제거
+    data_v = data_v.drop(['deg', 'deg/sec', 'mA', 'endpoint', 'torque', 'force', 'ori'], axis=1)
+
+    # 숫자 변환
+    data_v = data_v.apply(pd.to_numeric, errors='coerce').fillna(0)
+
+    # 'time' 열
+    data_v['time'] = data_v['timestamp'] - data_v['sequence'] - 1
+    data_v = data_v.drop(['sequence', 'timestamp'], axis=1)
+
+    # 시간 정렬
+    data_v.sort_values(by=['time'], ascending=True, inplace=True)
+    data_v.reset_index(drop=True, inplace=True)
+
+    # 스케일러 적용
+    if imported_scaler is not None:
+        arr_scaled = imported_scaler.transform(data_v.values)
+    else:
+        arr_scaled = scaler.fit_transform(data_v.values)
+    return torch.tensor(arr_scaled, dtype=torch.float32)
+
 ##########################
-# 두 개의 모델 학습
+# 실행
 ##########################
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -157,25 +181,19 @@ def main():
     torch.backends.cuda.matmul.allow_tf32 = True
     torch.backends.cudnn.allow_tf32 = True
 
-    ##########################
-    # 1. 분류
-
     # 데이터셋 생성
-    c_base_path = "data/all_data"  # TODO: 실제 폴더 경로로 수정
+    c_base_path = "data/all_data"  
     c_dataset = ClassificationDataset(c_base_path)
 
     # 2) 고유 라벨 목록 추출
-    global unique_labels
-    # unique_labels = sorted(list(set(c_dataset.labels)))
-    unique_labels = get_unique_labels(c_base_path)
-    print("Unique labels:", unique_labels)
+    print("Unique labels:", c_dataset.unique_labels)
 
     # 3) 하이퍼파라미터 설정
     input_dim = 21  # 전처리 후 실제 피처 수
     d_model = 32
     nhead = 2
-    num_layers = 1
-    num_classes = len(unique_labels)
+    num_layers = 3
+    num_classes = len(c_dataset.unique_labels)
     num_epochs = 100
     batch_size = 32
 
@@ -185,17 +203,13 @@ def main():
     test_size = len(c_dataset) - train_size - val_size
 
     # Classification DataLoader
-    classification_dataset = ClassificationDataset(c_base_path)
-    classification_train_dataset, classification_val_dataset, classification_test_dataset = random_split(classification_dataset, [train_size, val_size, test_size])
-    # classification_train_loader = DataLoader(classification_train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
-    # classification_val_loader = DataLoader(classification_val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
-    # classification_test_loader = DataLoader(classification_test_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
-    collate_fn_with_base_path = partial(collate_fn, base_path=c_base_path)
+    collate_fn_with_labels = partial(collate_fn, dataset=c_dataset)
+    classification_train_dataset, classification_val_dataset, classification_test_dataset = random_split(c_dataset, [train_size, val_size, test_size])
 
-    # DataLoader에서 collate_fn을 partial로 전달
-    classification_train_loader = DataLoader(classification_train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn_with_base_path)
-    classification_val_loader = DataLoader(classification_val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn_with_base_path)
-    classification_test_loader = DataLoader(classification_test_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn_with_base_path)
+    # DataLoader에서 collate_fn을 partial로 전달    
+    classification_train_loader = DataLoader(classification_train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn_with_labels)
+    classification_val_loader = DataLoader(classification_val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn_with_labels)
+    classification_test_loader = DataLoader(classification_test_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn_with_labels)
 
     # 분류 모델/손실함수/옵티마이저
     model = TransformerModel(input_dim, d_model, nhead, num_layers, num_classes).to(device)
@@ -225,90 +239,17 @@ def main():
         test_file_path = os.path.join(non_golden_dir, selected_file)
 
         best_model.eval()
-        single_data = load_and_preprocess_trajectory(test_file_path).unsqueeze(0).to(device)
+        single_data = load_and_preprocess_trajectory(test_file_path, imported_scaler=c_dataset.scaler).unsqueeze(0).to(device)
         with torch.no_grad():
             out = best_model(single_data)
             _, pred_idx = torch.max(out, 1)
         # pred_label = get_unique_labels[pred_idx.item()]
-        pred_label = unique_labels[pred_idx.item()]
+        pred_label = c_dataset.unique_labels[pred_idx.item()]
 
         print(f"\n=== Random Non-Golden Prediction ===")
         print(f"Selected file: {selected_file}")
         print(f"Predicted label: {pred_label}")
-    ##########################
-
-    ##########################
-    # 2. 생성
-
-    g_data_path = "data"
-    analyzer = TrajectoryAnalyzer(g_data_path)
-    generator = TrajectoryGenerator()
-
-    try:
-        movement_types = analyzer.get_available_movement_types()
-        print("\nTrajectory List", movement_types)
-        
-        if movement_types:
-            # 랜덤하게 동작 타입 선택
-            movement_type = random.choice(movement_types)
-            
-            # golden sample에서 데이터 가져오기
-            target_traj, _ = analyzer.get_random_trajectory_by_type(
-                analyzer.golden_dir, movement_type
-            )
-            
-            # dataset 생성
-            dataset = GenerationDataset(target_traj)
-            
-            # 데이터로더 생성
-            g_train_loader, g_val_loader, g_test_loader = generation_dataloaders(
-                target_traj,  # DataFrame 전달
-                batch_size=32
-            )
-            
-            # LSTM 모델 학습
-            print("\nTraining LSTM Start...")
-            model, history, scaler = train_generation(
-                analyzer=analyzer,
-                movement_type=movement_type,
-                train_loader=g_train_loader,
-                val_loader=g_val_loader,  
-                num_samples=5,
-                epochs=100
-            )
-            
-            # 테스트용 궤적 로드
-            print("\nLoad Test Trajectory...")
-            target_traj, user_traj = analyzer.load_random_trajectories(movement_type)
-            
-            # LSTM으로 예측
-            print("\nTrajectory Prediction...")
-            test_batch = next(iter(g_test_loader))
-            sequence, _ = test_batch
-            sequence = sequence.to(device)
-            
-            model.eval()
-            with torch.no_grad():
-                predicted = model(sequence)
-                predicted = predicted.cpu().numpy()
-                predicted = scaler.inverse_transform(predicted)
-                
-                print("Prediction Position:", predicted[0, :3])
-            
-            # 시각화 비교
-            print("\nTrajectory Comparison Visualization...")
-            aligned_trajectory = generator.compare_trajectories(target_traj, user_traj)
-            
-            # 학습 결과 출력
-            print("\nTraining Results:")
-            print(f"Final train loss: {history['train_loss'][-1]:.4f}")
-            print(f"Final validation loss: {history['val_loss'][-1]:.4f}")
-            
-        else:
-            print("사용 가능한 동작 타입이 없습니다.")
-            
-    except Exception as e:
-        print(f"오류 발생: {str(e)}")
+        print("\nCompleted Classification Model")
 
 if __name__ == "__main__":
     main()
